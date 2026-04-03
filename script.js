66f7e39be0b32eaca96b79d6888cfeaedcbb6fde
hnanna2 <hnanna2@illinois.edu>
2026-04-02 21:05:24 -0500

    Initial website upload

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Portfolio loaded!");
+
+document.querySelectorAll('.project-swiper').forEach(swiperEl => {
+  new Swiper(swiperEl, {
+    loop: true,
+    spaceBetween: 20,
+    slidesPerView: 1,
+    pagination: {
+      el: swiperEl.querySelector('.swiper-pagination'),
+      clickable: true
+    },
+    navigation: {
+      nextEl: swiperEl.querySelector('.swiper-button-next'),
+      prevEl: swiperEl.querySelector('.swiper-button-prev')
+    }
+  });
+});
